b32a57ad0ec3cec81974305a9753bce14703dc7c
esgolubeva <lena19948@gmail.com>
2019-11-04 18:07:13 +0100

    Change AuthProvider

--- src/auth/AuthContext.jsx
+++ src/auth/AuthContext.jsx
@@ -1,26 +1,27 @@
-import React from "react";
-
-export class AuthHandler {
-	constructor() {
-		this.isLoggedIn = false;
-	}
-
-	login = (username, password) => {
-		this.isLoggedIn = true;
-	};
-
-	logout = () => {
-		this.isLoggedIn = false;
-	};
-}
-
-const defaultAuthHandler = new AuthHandler();
+import React, { useState } from "react";
 
 export const AuthContext = React.createContext();
 
 export const AuthProvider = props => {
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+	const login = (username, password) => {
+		setIsLoggedIn(true);
+		console.log(isLoggedIn);
+	};
+
+	const logout = () => {
+		setIsLoggedIn(false);
+		console.log(isLoggedIn);
+	};
 	return (
-		<AuthContext.Provider value={defaultAuthHandler}>
+		<AuthContext.Provider
+			value={{
+				isLoggedIn,
+				login,
+				logout
+			}}
+		>
 			{props.children}
 		</AuthContext.Provider>
 	);
