34c2be28b573b2f36f4afcec01ae464047569bae
esgolubeva <lena19948@gmail.com>
2019-11-11 23:31:09 +0100

    test LoginForm

--- src/components/login/LoginForm.test.js
+++ src/components/login/LoginForm.test.js
@@ -1,24 +1,27 @@
 import React from "react";
 import { fireEvent, wait } from "@testing-library/react";
 import { App } from "../../App";
-import LoginForm from "./LoginForm";
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../../modules";
 import { fetchAuthRequest, fetchAuthSuccess } from "../../modules/auth";
 
 describe("LoginForm", () => {
-	it("should submit form on button click and redirect to map page", () => {
+	it("should on Войти button click redirect to map page", () => {
 		let store = createStore(
 			rootReducer,
 			applyMiddleware(store => next => action => {
 				if (action.type === fetchAuthRequest.toString()) {
-					return Promise.resolve();
+					expect(action.payload).toStrictEqual({
+						email: "email@example.com",
+						password: "password"
+					});
+					return store.dispatch(fetchAuthSuccess());
 				}
 				return next(action);
 			})
 		);
 
-		let { getByTestId} = renderWithProviders(<App />, store);
+		let { getByTestId } = renderWithProviders(<App />, store);
 
 		fireEvent.change(getByTestId("inputName"), {
 			target: { value: "email@example.com" }
@@ -30,7 +33,7 @@ describe("LoginForm", () => {
 
 		let loginButton = getByTestId("buttonLogin");
 		fireEvent.submit(loginButton);
-		jest.setTimeout(10000);
-		return wait(() => (getByTestId("map"));
+
+		return wait(() => getByTestId("map"));
 	});
 });
