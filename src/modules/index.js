8bb2ac59ba0e636dc8d98fb0b097b8960f224529
esgolubeva <lena19948@gmail.com>
2019-11-25 23:27:02 +0100

    reset state after logout

--- src/modules/index.js
+++ src/modules/index.js
@@ -3,10 +3,19 @@ import auth from "./auth";
 import card from "./card";
 import address from "./address";
 import route from "./route";
+import { fetchLogout } from "./auth/actions";
 
-export default combineReducers({
+const rootReducer = combineReducers({
 	auth,
 	card,
 	address,
 	route
 });
+
+export default (state, action) => {
+	if (action.type === fetchLogout.toString()) {
+		state = undefined;
+	}
+
+	return rootReducer(state, action);
+};
